b70679ccc6f8b92ecfac41c9bb353dc0b2864a4f
EryX666 <omerbar7193@gmail.com>
2022-04-06 02:46:01 +0300

    added store
    Signed-off-by: EryX666 <omerbar7193@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './utils/reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./utils/reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { SnackbarProvider } from 'notistack';
-import Theme from './theme'
+import { SnackbarProvider } from "notistack";
+import Theme from "./theme";
+import { store } from "stateStore/store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={Theme}>
-        <CssBaseline />
-        <SnackbarProvider maxSnack={3} >
-          <App />
-        </SnackbarProvider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<ThemeProvider theme={Theme}>
+					<CssBaseline />
+					<SnackbarProvider maxSnack={3}>
+						<App />
+					</SnackbarProvider>
+				</ThemeProvider>
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 reportWebVitals();
